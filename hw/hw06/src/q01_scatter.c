3d22fc874ebb7f8c1cab1c678de691994fc7ca18
bardi <bardia.mojra@gmail.com>
2022-06-28 08:41:40 -0500

    Updated with git-express on 2022-06-28.

--- hw/hw06/src/q01_scatter.c
+++ hw/hw06/src/q01_scatter.c
@@ -7,7 +7,7 @@
 /* config */
 #define ROOT 0
 
-void print_arrBuff(int rank, int size, int* arr_ptr, MPI_Comm comm);
+void print_buff(int rank, int size, int** arr_ptr, int arr_len);
 
 int main(int argc, char *argv[])
 {
@@ -18,42 +18,43 @@ int main(int argc, char *argv[])
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Request request;
   int arr_len;
-  int* arr_ptr;
+  int* arr_ptr; // global pointer
   double t1, t2;
 
-
-  if size != 2 || size != 4 || size != 8 || size != 16
-  {
-    printf("\n\nUnsupported number of processors: %d\n", size);
-    exit(0);
+  if (rank == ROOT) {
+    if ((size != 2) && (size != 4) && (size != 8) && (size != 16)) {
+      printf("\n\nUnsupported number of processors: %d\n\n", size);
+      exit(13);
+    }
   }
 
-  if (rank == ROOT)
-  {
-    printf("[%2d/%2d]: <--> [rank/size]\n", rank, size);
+  /* NBUG */
+  arr_len = rank;
+  printf("[%2d/%2d]: init int arr size: %d\n", rank, size, arr_len); fflush(stdout);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  if (rank == ROOT) {
+    printf("[%2d/%2d]: <=> :[rank/size]\n", rank, size);
     printf("[%2d/%2d]: Enter an integer array length: \n", rank, size);
     scanf("%d", &arr_len);
     printf("[%2d/%2d]: int array length = %d\n", rank, size, arr_len);
 
+    arr_ptr = (int*) malloc(sizeof(int) * arr_len);
+    memset(arr_ptr, 7, arr_len); // <---- init all buffers with 7
+
+    printf("[%2d/%2d]: initial array buffer values \n", rank, size);
+    print_buff(rank, size, &arr_ptr, arr_len);
+
   }
-
-  // NBUG
-  arr_len = rank;
-  printf("[%2d/%2d]: int arr size: %d\n", rank, size, arr_len); fflush(stdout);
   MPI_Barrier(MPI_COMM_WORLD);
 
-  MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Bcast(arr_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  // NBUG
-  printf("[%2d/%2d]: int arr size: %d\n", rank, size, arr_len); fflush(stdout);
+  MPI_Bcast(&arr_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Barrier(MPI_COMM_WORLD);
 
+  /* NBUG */
+  printf("[%2d/%2d]: int arr len: %d\n", rank, size, arr_len); fflush(stdout);
+  MPI_Barrier(MPI_COMM_WORLD);
 
-  arr_ptr = (int*) malloc(sizeof(int) * arr_len);
-  memset(arr_ptr, 7, sizeof(int) * arr_len); // <---- init all buffers with 7
-  print_arr_all(rank, size, arr_ptr, arr_len, MPI_COMM_WORLD);
 
 
 
@@ -61,13 +62,20 @@ int main(int argc, char *argv[])
     for (int i=0; i<arr_len; i++){
       arr_ptr[i] = i+10;
     }
+    printf("[%2d/%2d]: before scatter arr buff values \n", rank, size);
+    print_buff(rank, size, &arr_ptr, arr_len);
     t1 = MPI_Wtime();
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Scatter(arr_ptr, 1, MPI_INT, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+  printf("[%2d/%2d]: int arr len: %d\n", rank, size, arr_len); fflush(stdout);
   MPI_Barrier(MPI_COMM_WORLD);
-  print_arr_all(rank, size, arr_ptr, arr_len, MPI_COMM_WORLD);
+
+  int *rec = NULL;
+	MPI_Scatter(arr_ptr, 1, MPI_INT, rec, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  print_buff(rank, size, &arr_ptr, arr_len);
   MPI_Barrier(MPI_COMM_WORLD);
 
 
@@ -78,28 +86,21 @@ int main(int argc, char *argv[])
   }
   MPI_Barrier(MPI_COMM_WORLD);
   fflush(stdout);
+  free(arr_ptr);
   MPI_Finalize();
-
-
   return 0;
 }
 
 
-// private routines
-void print_arr_all(rank, size, arr_ptr, arr_len, comm)
-{
+/* private routines */
+void print_buff(int rank, int size, int** arr_ptr, int arr_len) {
   /* print arr_ptr */
-  for (int i=0; i<size; i++) {
-    MPI_Barrier(comm);
-    if(i == rank)
-    {
-      printf("[%2d/%2d]: ", rank, size);
-      for (int j=0; j<arr_len; j++) {
-      printf("%3d ", arr_ptr[j]);
-      } printf("\n"); fflush(stdout);
-    }
-    MPI_Barrier(comm);
-  }
-  MPI_Barrier(comm);
-  return 0;
+  MPI_Barrier(MPI_COMM_WORLD);
+  if(rank == ROOT) {
+    printf("[%2d/%2d]: ", rank, size);
+    for (int j=0; j<arr_len; j++) {
+    printf("%3d ", arr_ptr[j]);
+    } printf("\n"); fflush(stdout);
+  } MPI_Barrier(MPI_COMM_WORLD);
+  return;
 }
