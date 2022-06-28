85f72174680e1c809c3ef9c528a606db8dff2f56
bardi <bardia.mojra@gmail.com>
2022-06-28 11:14:32 -0500

    Updated with git-express on 2022-06-28.

--- hw/hw06/src/q01_scatter.c
+++ hw/hw06/src/q01_scatter.c
@@ -1,4 +1,5 @@
 #include <mpi.h>
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,7 +8,9 @@
 /* config */
 #define ROOT 0
 
-void print_buff(int rank, int size, int** arr_ptr, int arr_len);
+int* create_gBuff(int rank, int size, int* arrLen);
+void print_buff(int rank, int size, int* buff, int arrLen);
+void print_arrLen(int rank, int size, int arrLen);
 
 int main(int argc, char *argv[])
 {
@@ -17,8 +20,8 @@ int main(int argc, char *argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Request request;
-  int arr_len;
-  int* arr_ptr; // global pointer
+  int arrLen = 0;
+  arrLen = rank;
   double t1, t2;
 
   if (rank == ROOT) {
@@ -28,79 +31,90 @@ int main(int argc, char *argv[])
     }
   }
 
-  /* NBUG */
-  arr_len = rank;
-  printf("[%2d/%2d]: init int arr size: %d\n", rank, size, arr_len); fflush(stdout);
-  MPI_Barrier(MPI_COMM_WORLD);
 
+  int* gBuff = NULL;
   if (rank == ROOT) {
-    printf("[%2d/%2d]: <=> :[rank/size]\n", rank, size);
-    printf("[%2d/%2d]: Enter an integer array length: \n", rank, size);
-    scanf("%d", &arr_len);
-    printf("[%2d/%2d]: int array length = %d\n", rank, size, arr_len);
-
-    arr_ptr = (int*) malloc(sizeof(int) * arr_len);
-    memset(arr_ptr, 7, arr_len); // <---- init all buffers with 7
-
-    printf("[%2d/%2d]: initial array buffer values \n", rank, size);
-    print_buff(rank, size, &arr_ptr, arr_len);
-
+    gBuff = create_gBuff(rank, size, &arrLen);
   }
-  MPI_Barrier(MPI_COMM_WORLD);
 
-  MPI_Bcast(&arr_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Bcast(&arrLen, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+  print_arrLen(rank, size, arrLen);
 
-  /* NBUG */
-  printf("[%2d/%2d]: int arr len: %d\n", rank, size, arr_len); fflush(stdout);
-  MPI_Barrier(MPI_COMM_WORLD);
+  int* lBuff = NULL;
+  lBuff = (int*) malloc(sizeof(int) * arrLen);
+  assert(lBuff != NULL);
+  memset(lBuff, 0, arrLen*sizeof(int));
+  print_buff(rank, size, lBuff, arrLen);
 
 
 
 
   if(rank == ROOT){
-    for (int i=0; i<arr_len; i++){
-      arr_ptr[i] = i+10;
+    for (int i=0; i<arrLen*size; i++){
+      gBuff[i] = i+1;
     }
     printf("[%2d/%2d]: before scatter arr buff values \n", rank, size);
-    print_buff(rank, size, &arr_ptr, arr_len);
+    print_buff(rank, size, gBuff, arrLen*size);
     t1 = MPI_Wtime();
   }
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  printf("[%2d/%2d]: int arr len: %d\n", rank, size, arr_len); fflush(stdout);
   MPI_Barrier(MPI_COMM_WORLD);
 
-  int *rec = NULL;
-	MPI_Scatter(arr_ptr, 1, MPI_INT, rec, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  print_buff(rank, size, lBuff, arrLen);
+
+
+
+	MPI_Scatter(gBuff, arrLen, MPI_INT, lBuff, arrLen, MPI_INT, ROOT, MPI_COMM_WORLD);
 
   MPI_Barrier(MPI_COMM_WORLD);
-  print_buff(rank, size, &arr_ptr, arr_len);
+  print_buff(rank, size, lBuff, arrLen);
   MPI_Barrier(MPI_COMM_WORLD);
-
+  print_buff(rank, size, lBuff, arrLen);
+  MPI_Barrier(MPI_COMM_WORLD);
 
   if (rank == ROOT)
   {
     t2 = MPI_Wtime();
     printf( "Elapsed time:     %f\n", t2-t1);
+    free(gBuff);
   }
+
   MPI_Barrier(MPI_COMM_WORLD);
   fflush(stdout);
-  free(arr_ptr);
+
+
+  free(lBuff);
   MPI_Finalize();
   return 0;
 }
 
 
 /* private routines */
-void print_buff(int rank, int size, int** arr_ptr, int arr_len) {
-  /* print arr_ptr */
-  MPI_Barrier(MPI_COMM_WORLD);
+void print_buff(int rank, int size, int* buff, int arrLen) {
   if(rank == ROOT) {
     printf("[%2d/%2d]: ", rank, size);
-    for (int j=0; j<arr_len; j++) {
-    printf("%3d ", arr_ptr[j]);
+    for (int j=0; j<arrLen; j++) {
+    printf("%3d ", buff[j]);
     } printf("\n"); fflush(stdout);
-  } MPI_Barrier(MPI_COMM_WORLD);
+  }
   return;
+}
+
+
+int* create_gBuff(int rank, int size, int* arrLen) {
+  printf("[%2d/%2d]: <=> :[rank/size]\n", rank, size);
+  printf("[%2d/%2d]: Enter the array length: \n", rank, size);
+  scanf("%d", arrLen);
+  printf("[%2d/%2d]: int array length = %d\n", rank, size, *arrLen);
+  int* gBuff = (int*) malloc(sizeof(int)*((*arrLen)*size));
+  assert(gBuff != NULL);
+  memset(gBuff, 0, size*sizeof(*gBuff));
+  print_buff(rank, size, gBuff, *arrLen);
+  return gBuff;
+}
+
+
+void print_arrLen(int rank, int size, int arrLen) {
+  printf("[%2d/%2d]: int arr len: %d\n", rank, size, arrLen); fflush(stdout);
+  MPI_Barrier(MPI_COMM_WORLD);
 }
